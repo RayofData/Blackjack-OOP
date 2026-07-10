f09857a8e342a0d24c5a6ddb9c963822c75c28fb
Ray Hobbs <170390763+Lovjoy@users.noreply.github.com>
2026-07-10 16:28:07 -0400

    started dealer object

---
+++ Dealer.py
@@ -0,0 +1,9 @@
+from Player import Player
+
+class Dealer(Player):
+    def __init__(self, name, cards, seat=1):
+        super().__init__(name, cards, seat)
+
+    def __str__(self):
+        cards_text = ", ".join(str(card) for card in self.cards)
+        return f"Dealer: {self.name}, Cards: {cards_text}"
