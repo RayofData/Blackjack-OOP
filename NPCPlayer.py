948edc245fed42f609b4469044da816842824a1f
Ray Hobbs <170390763+Lovjoy@users.noreply.github.com>
2026-07-10 16:29:06 -0400

    created basic npc with betting

---
+++ NPCPlayer.py
@@ -0,0 +1,30 @@
+from Player import Player
+import random
+
+class NPCPlayer(Player):
+    def __init__(self, name, cards, seat, total, bet=0):
+        super().__init__(name, cards, seat)
+        self.total = total
+        self.bet = bet
+
+    def __str__(self):
+        cards_text = ", ".join(str(card) for card in self.cards)
+        return f"NPC: {self.name}, Cards: {cards_text}, Pot: {self.total}"
+
+    def place_bet(self):
+        self.bet = random.randint(1,3)*5
+        self.total -= self.bet
+    
+    def double_down(self, deck):
+        self.bet *= 2
+        self.receive_card(deck.pop())
+
+    def action(self, deck):
+        if sum(self.cards.values) < 9:
+            self.hit(deck)
+        elif sum(self.cards.values) < 12:
+            self.double_down(deck)
+        elif sum(self.cards.values) < 16:
+            self.hit(deck)
+        else:
+            self.stay() 
