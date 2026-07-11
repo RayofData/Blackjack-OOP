7e62f6f9a5226233b96bef892f0c913162497a4f
Ray Hobbs <170390763+Lovjoy@users.noreply.github.com>
2026-07-11 15:17:23 -0400

    Fix table size

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ table = Table([dealer])
 
 ##### Welcome Message ######
 print("Welcome to Ray's Blackjack!")
-print("This table has five available seats.")
+print(f"This table has {table.table_size - table.current_table_size()} available seats.")
 
 print("The game ends when you leave the table or run out of money.")
 print("Each player begins with the same starting balance.")
@@ -35,7 +35,7 @@ starting_balance = int(
     input("Enter the starting balance for each player: $")
 )
 
-##### Play creation #####
+##### Players creation #####
 # Human players
 num_players = int(input("How many human players are joining? "))
 for i in range(num_players):
@@ -46,7 +46,7 @@ for i in range(num_players):
     if len(table.persons) == table.table_size:
         break
 
-# Npc players
+# NPC players
 max_npcs = table.table_size - table.current_table_size()
 num_npcs = int(input(f"How many NPC players are joining? Maximum: {max_npcs} "))
 npc_names = [
@@ -83,58 +83,58 @@ while table.current_table_size() < table.table_size and i < num_npcs:
 
 
 ## Start game loop
+while table.current_table_size() > 1:
+        
+    # Everyone at table
+    table.print_table()
 
-# Everyone at table
-table.print_table()
+    # Stars new game with new shuffled deck
+    play_deck = deck.copy()
+    random.shuffle(play_deck)
 
-# Stars new game with new shuffled deck
-play_deck = deck.copy()
-random.shuffle(play_deck)
+    # Human Bet
+    for person in table.persons[1:]:
+        person.ask_bet()
+    
 
-# Bet
-for person in table.persons[1:]:
-    person.place_bet()
+        
 
-# Deal cards
-for i in range(2):
-    for j in range(table.current_table_size()):
-        current_player = table.persons[j]
-        card = play_deck.pop()
+    # Deal cards
+    for i in range(2):
+        for j in range(table.current_table_size()):
+            current_player = table.persons[j]
+            card = play_deck.pop()
 
-        if i == 0 and j == 0: 
-            current_player.receive_card(card) 
-        else:
-            card.state = "show"
-            current_player.receive_card(card)
+            if i == 0 and j == 0: 
+                current_player.receive_card(card) 
+            else:
+                card.state = "show"
+                current_player.receive_card(card)
 
-# Everyone at table
-table.print_table()
-dealer_seat = table.persons[0]
+    # Everyone at table
+    table.print_table()
+    dealer_seat = table.persons[0]
 
-# Check for Dealer Blackjack
-hidden_card = dealer_seat.cards[0]
-visible_card = dealer_seat.cards[1]
+    ##### Check for Dealer Blackjack #####
+    hidden_card = dealer_seat.cards[0]
+    visible_card = dealer_seat.cards[1]
 
-# Insurance for dealer blackjack
-if visible_card.get_rank == "Ace":
-    pass # insurance bet
+    # Insurance for dealer blackjack
+    if visible_card.get_rank == "Ace":
+        pass # insurance bet to be set up
+    
+    # Check for dealer blackjack
+    if (hidden_card.ace_check() and visible_card.get_value() == 10) or (hidden_card.ten_check and visible_card.get_value() == 11):
+        print("Dealer Blackjack! No one wins.")
+        for i in range(1, table.current_table_size()):
+            current_player = table.persons[i]
+            if current_player.get_hand_total == 21:
+                current_player.total += current_player.bet
+            current_player.bet = 0   
 
-if (hidden_card.ace_check() and visible_card.get_value() == 10) or (hidden_card.ten_check and visible_card.get_value() == 11):
-    print("Dealer Blackjack! No one wins.")
+    ##### Actions for hit, stay, double down and split #####
     for i in range(1, table.current_table_size()):
         current_player = table.persons[i]
-        if current_player.get_hand_total == 21:
-            current_player.total += current_player.bet
-        current_player.bet = 0   
-
-
-
-
-
-
-# Actions for hit, stay, double down and split
-for i in range(1, table.current_table_size()):
-    current_player = table.persons[i]
-    print(dealer_seat)
-    print(current_player)
-    current_player.action(play_deck)
+        print(dealer_seat)
+        print(current_player)
+        current_player.action(play_deck)
