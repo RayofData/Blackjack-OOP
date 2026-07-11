c4ab6289ebcfe3a9197c657213cc0e205a2718bc
Ray Hobbs <170390763+Lovjoy@users.noreply.github.com>
2026-07-11 15:18:46 -0400

    created new sub class of player and parent class or Human and NPC players

---
+++ BettingPlayer.py
@@ -0,0 +1,25 @@
+from Player import Player
+
+class BettingPlayer(Player):
+    def __init__(self, name, seat, total, cards=None, hand_total=0, bet=0):
+        if cards is None:
+            cards = []
+
+        super().__init__(name, cards, seat, hand_total)
+        self.total = total
+        self.bet = bet 
+           
+    def ask_bet(self):
+        pass
+
+    def place_bet(self, bet):
+        self.bet = bet
+        self.total -= self.bet 
+
+    def split(self, deck):
+        pass
+        # split into two hands and double bet 
+
+    def double_down(self, deck):
+        self.bet *= 2
+        self.hit(deck) 
