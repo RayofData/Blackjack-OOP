a439d7f8b8c4f6dfa493a2fd0670f6490512f9f1
Ray Hobbs <170390763+Lovjoy@users.noreply.github.com>
2026-07-12 14:30:20 -0400

    fix methods for new hand object

--- Dealer.py
+++ Dealer.py
@@ -1,15 +1,19 @@
 from Player import Player
+from styling import colored_text, RED, GREEN, YELLOW, BLUE
 
 class Dealer(Player):
     def __init__(self, name, hands, seat=1):
         super().__init__(name, hands, seat)
 
     def print_hand(self):
-        print(f"Dealer: {self.name}")
+        print(colored_text(f"Dealer: {self.name}", BLUE))
         current_hand = self.hands[0]
-        print(f"Cards: {current_hand.get_hand_text()}\t{current_hand.get_hand_total_text()}")
+        print(colored_text(f"Cards: {current_hand.get_hand_text()}\t{current_hand.get_hand_total_text()}", YELLOW))
 
     def action(self, deck):
-        self.set_hand_total()
-        while self.get_hand_total() < 17:
+        hand = self.hands[0]
+        total = hand.get_total()
+        while total < 17:
             self.hit(deck)
+            hand = self.hands[0]
+            total = hand.get_total()
