e78148c4ad291f42cf7528502dab0f41850fa578
Ray Hobbs <170390763+Lovjoy@users.noreply.github.com>
2026-07-12 13:35:05 -0400

    added print_hand method

--- Dealer.py
+++ Dealer.py
@@ -4,10 +4,10 @@ class Dealer(Player):
     def __init__(self, name, hands, seat=1):
         super().__init__(name, hands, seat)
 
-    def __str__(self):
+    def print_hand(self):
         print(f"Dealer: {self.name}")
         current_hand = self.hands[0]
-        print(f"Cards: {current_hand.get_hand}\t{current_hand.get_total()}")
+        print(f"Cards: {current_hand.get_hand_text()}\t{current_hand.get_hand_total_text()}")
 
     def action(self, deck):
         self.set_hand_total()
