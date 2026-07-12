091336f879c61518e22cbe2d8080f851687759f6
Ray Hobbs <170390763+Lovjoy@users.noreply.github.com>
2026-07-12 10:10:51 -0400

    added new methods for totals

--- Hand.py
+++ Hand.py
@@ -1,11 +1,18 @@
 from Card import Card
 
 class Hand(Card):
-    def __init__(self, cards):
+    def __init__(self, cards, hand_total=0):
         self.cards = cards
+        self.__hand_total = hand_total
 
     def add_card(self, card):
         self.cards.append(card)
 
     def clear_hand(self):
         self.cards = []
+
+    def set_total(self):
+        self.__hand_total = sum(card.get_value() for card in self.cards)
+
+    def get_total(self):
+        return self.__hand_total
