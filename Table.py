97b30d0bada574902fd276a957358ac541805f14
Ray Hobbs <170390763+Lovjoy@users.noreply.github.com>
2026-07-11 21:10:34 -0400

    added leave print statement with total pot

--- Table.py
+++ Table.py
@@ -1,3 +1,5 @@
+from styling import colored_text, RED, GREEN, YELLOW, BLUE
+
 class Table:
     def __init__(self, persons=None, table_size=6):
         self.persons = [] if persons is None else persons
@@ -10,7 +12,12 @@ class Table:
             print("Table is full.")
 
     def leave_table(self, seat):
-        self.persons.pop(seat-1)
+        person = self.persons[seat - 1]
+        if person.total < 5:
+            print(colored_text(f"{person.name} LOST.", RED))
+        else:
+            print(colored_text(f"{person.name} leaves with ${person.total}.", YELLOW))
+        self.persons.pop(seat - 1)
 
     def print_table(self):
         for i in range(len(self.persons)):
